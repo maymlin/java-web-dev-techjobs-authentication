a8586b0fb6b0c798432b548254a548c3b190ea7b
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-30 22:20:11 -0500

    Complete 19.6.2 - add User entity, UserRepository; complete 19.6.3 #1~5 - add DTOs (loginForm and registrationForm), add AuthenticationController to handle registration form (display, process), login form (display only)

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -0,0 +1,96 @@
+package org.launchcode.javawebdevtechjobsauthentication.controllers;
+
+import org.launchcode.javawebdevtechjobsauthentication.data.UserRepository;
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.dto.LoginFormDTO;
+import org.launchcode.javawebdevtechjobsauthentication.models.dto.RegistrationFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final String sessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(sessionKey);
+
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    public static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(sessionKey, user.getId());
+    }
+
+    @GetMapping("register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegistrationFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+    @PostMapping("register")
+    public String processRegistrationForm(@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO,
+                                          Errors errors, HttpServletRequest request,
+                                          Model model) {
+
+        if (errors.hasErrors()) {
+            displayRegistrationForm(model);
+        }
+
+        User existingUser = userRepository.findByUsername(registrationFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyexists",
+                    "A user with that username already exists");
+            displayRegistrationForm(model);
+        }
+
+        String password = registrationFormDTO.getPassword();
+        String verifyPassword = registrationFormDTO.getVerifyPassword();
+
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "password.mismatch",
+                    "Passwords do not match");
+            displayRegistrationForm(model);
+        }
+
+        User newUser = new User(registrationFormDTO.getUsername(), password);
+
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+
+        return "redirect:";
+    }
+
+    @GetMapping("login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Login");
+
+        return "login";
+    }
+}
